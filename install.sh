7d9827650f785636c5c7ed87d9b89c192f060312
arytmw <54942957+arytmw@users.noreply.github.com>
2023-11-25 12:57:40 +0530

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
+sudo yum upgrade
+# Add required dependencies for the jenkins package
+sudo yum install fontconfig java-17-openjdk
+sudo yum install jenkins
+sudo systemctl daemon-reload
+# Start Services
+sudo systemctl enable jenkins
+sudo systemctl start jenkins
+# Firewall config
+sudo firewall-cmd --permanent --add-port=8080/tcp
+sudo firewall-cmd --reload
