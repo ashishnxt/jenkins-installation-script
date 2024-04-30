f5ce5aec27336101cf11f58be9aa1ee3fbff7e94
arytmw <54942957+arytmw@users.noreply.github.com>
2024-04-30 19:17:50 +0530

    Create install-deb.sh

---
+++ install-deb.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+sudo apt update -y
+sudo apt install default-jre -y
+sudo apt install default-jdk -y
+wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key |sudo gpg --dearmor -o /usr/share/keyrings/jenkins.gpg
+sudo sh -c 'echo deb [signed-by=/usr/share/keyrings/jenkins.gpg] http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+sudo apt update -y
+sudo apt install jenkins -y
+sudo systemctl start jenkins
+sudo systemctl enable jenkins
+sudo ufw allow 8080
+sudo cat /var/lib/jenkins/secrets/initialAdminPassword
